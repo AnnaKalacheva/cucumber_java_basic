85595fb82ac5863ec959653374a9c4ca2b414533
Student <dfdff>
2019-04-04 15:26:00 +0300

    First Cucumber task

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -1,9 +1,11 @@
 package stepDefinitions;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -38,12 +40,14 @@ public class SampleSteps {
                 driver.findElement(By.cssSelector("p")).getText());
     }
 
+    //capturing text, we need to define it in public void String or int or other
     @When("^I enter name: \"([^\"]*)\"$")
     public void iEnterName(String name) throws Throwable {
         driver.findElement(By.id("name")).clear();
         driver.findElement(By.id("name")).sendKeys(name);
     }
 
+    //capturing number
     @And("^I enter age: (\\d+)$")
     public void iEnterAge(int age) throws Throwable {
         driver.findElement(By.id("age")).sendKeys(String.valueOf(age));
@@ -63,7 +67,7 @@ public class SampleSteps {
     public void iSeeMessage(String message) throws Throwable {
         assertEquals(message, driver.findElement(By.id("message")).getText());
     }
-
+//it shows value or values
     @When("^I enter values:$")
     public void iEnterValues(Map<String, String> valuesToEnter) throws Throwable {
         for (Map.Entry<String, String> e : valuesToEnter.entrySet()) {
@@ -99,5 +103,80 @@ public class SampleSteps {
     @Given("^I am on action page$")
     public void iAmOnActionPage() {
         driver.get("https://kristinek.github.io/site/examples/actions");
+    }
+
+    @And("^I should see some text$")
+    public void iShouldSeeSomeText() {
+       assertTrue(driver.findElement(By.tagName("p")).isDisplayed());
+        System.out.println(driver.findElement(By.tagName("p")).getText());
+
+    }
+
+    @When("^I open styles page$")
+    public void iOpenStylesPage() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/styles");
+            }
+
+    @Then("^correct header is seen$")
+    public void correctHeaderIsSeen() throws Throwable {
+         assertEquals("Lorem ipsum", driver.findElement(By.className("w3-jumbo")).getText());
+
+    }
+
+    @Given("^I open action page$")
+    public void iOpenActionPage() throws Throwable {
+         driver.get("https://kristinek.github.io/site/examples/actions");
+    }
+
+
+    @And("^I enter number: (\\d+)$")
+    public void iEnterNumber(int number) throws Throwable {
+        driver.findElement(By.name("vfb-9")).clear();
+        driver.findElement(By.name("vfb-9")).sendKeys(String.valueOf(number));
+                  }
+
+    @And("^I click Result$")
+    public void iClickResult() throws Throwable {
+        driver.findElement(By.id("result_button_number")).click();
+    }
+
+    @Then("^I see Text: You entered number: \"([^\"]*)\"$")
+    public void iSeeTextYouEnteredNumber(String text) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        assertEquals("You entered number: \"5\"", driver.findElement(By.id("result_number")).getText());
+            }
+
+///IT IS FOR TASK1
+    @Given("^I am on number page$")
+    public void iAmOnNumberPage() throws Throwable {
+        driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
+    }
+
+    @When("^I enter too small number: \"([^\"]*)\"$")
+    public void iEnterNumberOnPage(int numberOnNumberPage) throws Throwable {
+        driver.findElement(By.id("numb")).clear();
+        driver.findElement(By.id("numb")).sendKeys(String.valueOf(numberOnNumberPage));
+    }
+
+    @And("^I click submit button$")
+    public void iClickSubmitButton() throws Throwable {
+        driver.findElement(By.xpath("//*[@class='w3-btn w3-orange w3-margin']")).click();
+    }
+
+    @Then("^I see message:\"([^\"]*)\"$")
+    public void iSeeMessageOnNumberPage(String message) throws Throwable {
+        assertEquals(message, driver.findElement(By.id("ch1_error")).getText());
+    }
+
+    @When("^I enter  number: \"([^\"]*)\"$")
+    public void iEnterNumberOnNumberPage(String numberOnNumberPage) throws Throwable {
+        driver.findElement(By.id("numb")).clear();
+        driver.findElement(By.id("numb")).sendKeys(numberOnNumberPage);
+    }
+
+    @Then("^I see alert:\"([^\"]*)\"$")
+    public void iSeeAlert(String oneAalert) throws Throwable {
+        Alert alert = driver.switchTo().alert();
+        assertEquals(oneAalert,alert.getText());
     }
 }
--- src/test/resources/features/Sample2.feature
+++ src/test/resources/features/Sample2.feature
@@ -3,6 +3,7 @@ Feature: Introduction to cucumber part 2
   I want to be able to write and execute a scenario with parameters
 
   Background:
+    //it is easier like step before, you can write in each scenario at the beginning
     Given I am on age page
 
   Scenario: a new scenario 1 with regex
@@ -16,3 +17,13 @@ Feature: Introduction to cucumber part 2
     And I enter age: 61
     And I click submit age
     Then I see message: "Hello, Bob, you are an adult"
+
+  Scenario: My 2nd scenario
+    Given  I open action page
+    And I enter number: 5
+    And I click Result
+    Then I see Text: You entered number: "5"
+
+
+
+
