18f8df11bd98764f9f19bf9981a666318a2f03bf
Ilze Rutenberga <ilzeruuta@inbox.lv>
2019-04-04 14:31:10 +0300

    commmit of Cucumber - task 1

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -1,9 +1,11 @@
 package stepDefinitions;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -44,12 +46,18 @@ public class SampleSteps {
         driver.findElement(By.id("name")).sendKeys(name);
     }
 
+    @When("^I enter name2: (.*)$")
+    public void iEnterName2(String name) throws Throwable {
+        driver.findElement(By.id("name")).clear();
+        driver.findElement(By.id("name")).sendKeys(name);
+    }
+
     @And("^I enter age: (\\d+)$")
     public void iEnterAge(int age) throws Throwable {
         driver.findElement(By.id("age")).sendKeys(String.valueOf(age));
     }
 
-    @Given("^I (?:am on|open) age page$")
+    @Given("^I (?:am on|open) age page$") //i am on page or i open page, "?" makes the last simbol before "?" optional
     public void iAmOnAgePage() throws Throwable {
         driver.get("https://kristinek.github.io/site/examples/age");
     }
@@ -74,8 +82,8 @@ public class SampleSteps {
         }
     }
 
-    @And("^I should see menu$")
-    public void iShouldSeeMenu() throws Throwable {
+    @And("^I should see menu$") // throws Throwable
+    public void iShouldSeeMenu() {
         assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
     }
 
@@ -99,5 +107,76 @@ public class SampleSteps {
     @Given("^I am on action page$")
     public void iAmOnActionPage() {
         driver.get("https://kristinek.github.io/site/examples/actions");
+    }
+
+    @And("^I should see some text$")
+    public void iShouldSeeSomeText() {
+        assertTrue(driver.findElement(By.tagName("p")).isDisplayed());
+        System.out.println(driver.findElement(By.tagName("p")).getText());
+    }
+
+    @When("^I open styles page$")
+    public void iOpenStylesPage() {
+        driver.get("https://kristinek.github.io/site/examples/styles");
+    }
+
+    @Then("^I should see styles page header$")
+    public void iShouldSeeStylesPageHeader() {
+        assertEquals("Lorem ipsum", driver.findElement(By.tagName("h1")).getText());
+    }
+
+    @When("^I open action page$")
+    public void iOpenActionPage() {
+       driver.get("https://kristinek.github.io/site/examples/actions");
+    }
+
+    @And("^I enter number: (\\d+)$")
+    public void iEnterNumber(int number) throws Throwable {
+        driver.findElement(By.id("number")).clear();
+        driver.findElement(By.id("number")).sendKeys(String.valueOf(number));
+    }
+
+    @And("^I click Result$")
+    public void iClickEnter() {
+        driver.findElement(By.id("result_button_number")).click();
+    }
+
+//   \"(.*)\"$") - to write input with quotes
+    @Then("^I see Text: (.*)$")
+    public void iSeeText(String text) throws Throwable {
+       assertEquals(text, driver.findElement(By.id("result_number")).getText());
+    }
+
+    @Given("^I (?:am on|open) number page$")
+    public void iAmOnNumberPage() throws Throwable {
+        driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
+    }
+
+    @When("^I enter a number: (.*)$")
+    public void iEnterANumber(String number) throws Throwable {
+        driver.findElement(By.id("numb")).clear();
+        driver.findElement(By.id("numb")).sendKeys(String.valueOf(number));
+    }
+
+    @And("^I click on Submit$")
+    public void iClickOnSubmit() {
+        driver.findElement(By.className("w3-btn")).click();
+    }
+
+    @Then("^I see result message: (.*)$")
+    public void iSeeResultMessage(String message) throws Throwable {
+        Alert alert = driver.switchTo().alert();
+        assertEquals(message, alert.getText());
+    }
+
+    @Then("^I click OK on result message$")
+    public void iClickOKOnResultMessage(){
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+    }
+
+    @Then("^I see error message: (.*)$")
+    public void iSeeErrorMessage(String text) throws Throwable {
+        assertEquals(text, driver.findElement(By.id("ch1_error")).getText());
     }
 }
